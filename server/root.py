894a8c6b4e5e210d30bf9334e7d9fa719581c8d9
Hunter <akulkaushal13579@gmail.com>
2025-12-19 18:51:04 +0530

    Ready for Render deployment

--- server/root.py
+++ server/root.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 from .Routes.upload import router as upload_router
 from .Routes.ask import router as ask_router
 
+from pathlib import Path
+from server.services.embeddings.build_index import build_index
+
+VECTOR_DIR = Path("vector_store")
+
+if not VECTOR_DIR.exists():
+    build_index()
+
+
 app = FastAPI()
 app.include_router(upload_router, tags=["Upload PDF"], prefix="/api")
 app.include_router(ask_router, tags=["Ask Question"], prefix="/api")
